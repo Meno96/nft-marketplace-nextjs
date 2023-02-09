6ab248fdf494c657350319412c0280ff232ddb5f
Daniele <danielemeno96@gmail.com>
2023-02-09 10:32:41 +0100

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,40 +1,10 @@
 /** @type {import('next').NextConfig} */
-
-const withOptimizedImages = require("next-optimized-images")
-module.exports = withOptimizedImages({
+const nextConfig = {
     reactStrictMode: true,
-    optimizeImages: true,
-    optimizeImagesInDev: false,
-    responsive: {
-        adapter: require("responsive-loader/sharp"),
+    images: {
+        loader: "akamai",
+        path: "",
     },
-})
+}
 
-// module.exports = {
-//     reactStrictMode: true,
-//     webpack: (config, options) => {
-//         // Use file-loader or url-loader for images
-//         config.module.rules.push({
-//             test: /\.(png|jpe?g|gif)$/i,
-//             use: [
-//                 {
-//                     loader: "file-loader",
-//                 },
-//             ],
-//         })
-
-//         // Use Babel, css-loader, and style-loader for CSS
-//         config.module.rules.push({
-//             test: /\.css$/,
-//             use: [
-//                 {
-//                     loader: "babel-loader",
-//                 },
-//                 "css-loader",
-//                 "style-loader",
-//             ],
-//         })
-
-//         return config
-//     },
-// }
+module.exports = nextConfig
